27b6558c36ed630719ba2124ce69555207dd53dd
Emanuele Galli <egalli64@gmail.com>
2016-12-20 20:16:35 +0100

    http://bitingcode.blogspot.com/2016/12/knapsack-without-repetition.html

--- fun/Knapsack.java
+++ fun/Knapsack.java
@@ -1,8 +1,9 @@
 /**
  * Knapsack.java
  * 
- * Dynamic programming solution to Knapsack problem
+ * Dynamic programming solution to Knapsack problems
  * More info: http://bitingcode.blogspot.com/2016/12/knapsack-with-repetitions.html
+ *            http://bitingcode.blogspot.com/2016/12/knapsack-without-repetition.html
  * 
  * @author manny egalli64@gmail.com
  */
@@ -10,20 +11,23 @@ package fun;
 
 public class Knapsack {
     /**
-     * Knapsack with repetition
-     * Based on algorithm discussed in chapter 6 of
+     * Knapsack with repetition Based on algorithm discussed in chapter 6 of
      * Algorithms by S. Dasgupta, C.H. Papadimitriou, and U.V. Vazirani
      * 
-     * more info: http://bitingcode.blogspot.com/2016/12/knapsack-with-repetitions.html
+     * more info:
+     * http://bitingcode.blogspot.com/2016/12/knapsack-with-repetitions.html
      * 
-     * @param values value associated to each item
-     * @param weights weight associated to each item
-     * @param weight weight the knapsack could carry
-     * @return the heighest value could be put in the knapsack
+     * @param values
+     *            value associated to each item
+     * @param weights
+     *            weight associated to each item
+     * @param weight
+     *            weight the knapsack could carry
+     * @return the highest value could be put in the knapsack
      */
     public static int withRepetition(int[] values, int[] weights, int weight) {
-        assert(values.length == weights.length);
-        
+        assert (values.length == weights.length);
+
         int[] results = new int[weight + 1];
         for(int i = 1; i < results.length; ++i) {
             for(int j = 0; j < weights.length; ++j) {
@@ -36,5 +40,37 @@ public class Knapsack {
         }
 
         return results[results.length - 1];
+    }
+
+    /**
+     * Knapsack without repetition
+     * 
+     * more info:
+     * http://bitingcode.blogspot.com/2016/12/knapsack-without-repetition.html
+     * 
+     * @param values
+     *            value associated to each item
+     * @param weights
+     *            weight associated to each item
+     * @param weight
+     *            weight the knapsack could carry
+     * @return the highest value could be put in the knapsack
+     */
+    public static int withoutRepetition(int[] values, int[] weights, int weight) {
+        assert (values.length == weights.length);
+
+        int[][] results = new int[values.length + 1][weight + 1];
+        for(int i = 1; i < results.length; ++i) {
+            for(int j = 1; j < results[i].length; ++j) {
+                results[i][j] = results[i - 1][j];
+                if(weights[i - 1] > j)
+                    continue;
+                int candidate = results[i - 1][j - weights[i - 1]] + values[i - 1];
+                if(candidate > results[i][j])
+                    results[i][j] = candidate;
+            }
+        }
+
+        return results[values.length][weight];
     }
 }
--- fun/KnapsackTest.java
+++ fun/KnapsackTest.java
@@ -3,6 +3,7 @@
  * 
  * Dynamic programming solution to Knapsack problem
  * More info: http://bitingcode.blogspot.com/2016/12/knapsack-with-repetitions.html
+ *            http://bitingcode.blogspot.com/2016/12/knapsack-without-repetition.html
  * 
  * @author manny egalli64@gmail.com
  */
@@ -16,11 +17,11 @@ import static org.hamcrest.core.Is.*;
 public class KnapsackTest {
 
     /**
-     * Knapsack with repetition
-     * Based on algorithm discussed in chapter 6 of
+     * Knapsack with repetition Based on algorithm discussed in chapter 6 of
      * Algorithms by S. Dasgupta, C.H. Papadimitriou, and U.V. Vazirani
      * 
-     * more info: http://bitingcode.blogspot.com/2016/12/knapsack-with-repetitions.html
+     * more info:
+     * http://bitingcode.blogspot.com/2016/12/knapsack-with-repetitions.html
      */
     @Test
     public void testWithRepetition() {
@@ -30,4 +31,17 @@ public class KnapsackTest {
         assertThat(Knapsack.withRepetition(values, weights, 10), is(48));
     }
 
+    /**
+     * Knapsack without repetition
+     * 
+     * more info:
+     * http://bitingcode.blogspot.com/2016/12/knapsack-without-repetition.html
+     */
+    @Test
+    public void testWithouRepetition() {
+        int[] weights = new int[] { 2, 3, 4, 6 };
+        int[] values = new int[] { 9, 14, 16, 30 };
+
+        assertThat(Knapsack.withoutRepetition(values, weights, 10), is(46));
+    }
 }
