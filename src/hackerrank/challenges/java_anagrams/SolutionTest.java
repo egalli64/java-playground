5637baabe7678c692e718082d4ee06062f7eac5c
Manny <egalli64@gmail.com>
2018-05-15 13:19:16 +0200

    HackerRank Java Strings: Java Anagrams

---
+++ src/hackerrank/challenges/java_anagrams/SolutionTest.java
@@ -0,0 +1,32 @@
+/**
+ * HackerRank Java Strings: Java Anagrams
+ * 
+ * @author: Manny egalli64@gmail.com
+ * @see: http://thisthread.blogspot.com/
+ *       https://www.hackerrank.com/challenges/equal/problem
+ */
+package hackerrank.challenges.java_anagrams;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+    @Test
+    void testGiven1() {
+        assertFalse(Solution.isAnagram("anagramm", "marganaa"));
+    }
+
+    @Test
+    void testGiven2() {
+        assertTrue(Solution.isAnagram("Hello", "hello"));
+    }
+
+    @Test
+    void testExtra() {
+        assertTrue(Solution.isAnagram("Abul", "BulA"));
+    }
+    
+    
+    
+}
