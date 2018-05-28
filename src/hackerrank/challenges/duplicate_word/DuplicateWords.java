e3b56795617bedb8b3f0eabe870a73e184caf5b3
Manny <egalli64@gmail.com>
2018-05-28 15:07:16 +0200

    HackerRank Java Strings: Java Regex 2 - Duplicate Words

---
+++ src/hackerrank/challenges/duplicate_word/DuplicateWords.java
@@ -0,0 +1,43 @@
+/**
+ * HackerRank Java Strings: Java Regex 2 - Duplicate Words
+ * 
+ * @author: Manny egalli64@gmail.com
+ * @see: http://thisthread.blogspot.com/
+ *       https://www.hackerrank.com/challenges/duplicate-word/problem
+ *
+ * Remove duplicated words from passed strings
+ * Using regex, pattern should be case insensitive
+ */
+package hackerrank.challenges.duplicate_word;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DuplicateWords {
+
+    public static void main(String[] args) {
+
+        String regex = "\\b(\\w+)(\\W+\\1\\b)+";
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+
+        Scanner in = new Scanner(System.in);
+        int numSentences = Integer.parseInt(in.nextLine());
+        
+        while (numSentences-- > 0) {
+            String input = in.nextLine();
+            
+            Matcher m = p.matcher(input);
+            
+            // Check for subsequences of input that match the compiled pattern
+            while (m.find()) {
+                input = input.replaceAll(m.group(), m.group(1));
+            }
+            
+            // Prints the modified sentence.
+            System.out.println(input);
+        }
+        
+        in.close();
+    }
+}
