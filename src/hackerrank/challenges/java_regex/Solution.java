db45dc8d2bf36017f7bc0359a9c0b03a0dbd508c
Manny <egalli64@gmail.com>
2018-05-28 14:31:50 +0200

    Extra-blanks removed from regex

--- src/hackerrank/challenges/java_regex/Solution.java
+++ src/hackerrank/challenges/java_regex/Solution.java
@@ -29,7 +29,7 @@ public class Solution {
 }
 
 class MyRegex {
-    static private String block = "(\\d{1,2} | (0|1)\\d{2} | 2[0-4]\\d | 25[0-5])";
+    static private String block = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
 
     String pattern = "(" + block + "\\.){3}" + block;
 }
