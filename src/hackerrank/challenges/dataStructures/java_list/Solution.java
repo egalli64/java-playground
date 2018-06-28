7d18f2992ceb680395bfc1aae15c314d75dad3c2
Manny <egalli64@gmail.com>
2018-06-28 14:28:24 +0200

    HackerRank Java Data Structures: Java List

---
+++ src/hackerrank/challenges/dataStructures/java_list/Solution.java
@@ -0,0 +1,38 @@
+/**
+ * HackerRank Java Data Structures: Java List
+ * 
+ * @author: Manny egalli64@gmail.com
+ * @see: http://thisthread.blogspot.com/
+ *       https://www.hackerrank.com/challenges/java-list/problem
+ *
+ * insert/delete integers from a list on demand
+ */
+package hackerrank.challenges.dataStructures.java_list;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        try(Scanner scanner = new Scanner(System.in)) {
+            ArrayList<Integer> data = new ArrayList<>();
+            int n = scanner.nextInt();
+            for(int i = 0; i < n; i++) {
+                data.add(scanner.nextInt());
+            }
+
+            for(int q = scanner.nextInt(); q > 0; q--) {
+                if(scanner.next().equals("Insert")) {
+                    data.add(scanner.nextInt(), scanner.nextInt());
+                } else {
+                    data.remove(scanner.nextInt());
+                }
+            }
+            
+            for(Integer value: data) {
+                System.out.print(value + " ");
+            }
+        }
+    }
+}
