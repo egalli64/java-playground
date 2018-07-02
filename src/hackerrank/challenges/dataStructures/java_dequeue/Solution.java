e11b2b4f6239ccc47859d96f8b01430d565b4f29
Manny <egalli64@gmail.com>
2018-07-02 14:21:45 +0200

    http://thisthread.blogspot.com/2018/07/hackerrank-java-dequeue.html

--- src/hackerrank/challenges/dataStructures/java_dequeue/Solution.java
+++ src/hackerrank/challenges/dataStructures/java_dequeue/Solution.java
@@ -2,15 +2,16 @@
  * HackerRank - Java - Data Structures - Java Dequeue
  * 
  * @author: Manny egalli64@gmail.com
- * @see: http://thisthread.blogspot.com/
+ * @see: http://thisthread.blogspot.com/2018/07/hackerrank-java-dequeue.html
  *       https://www.hackerrank.com/challenges/java-dequeue/problem
  *
  * n = number of integers to be read
  * m = window size
- * calculate the highest number of unique elements in each window 
+ * calculate the highest number of unique elements in any window 
  */
 package hackerrank.challenges.dataStructures.java_dequeue;
 
+import java.io.InputStream;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -18,14 +19,17 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
+    private static Deque<Integer> window = new LinkedList<>();
+    private static Map<Integer, Integer> counter = new HashMap<>();
+
+    public static int solution(InputStream is) {
+        try (Scanner scanner = new Scanner(is)) {
             int n = scanner.nextInt();
             int m = scanner.nextInt();
             assert m <= n;
 
-            Deque<Integer> window = new LinkedList<>();
-            Map<Integer, Integer> counter = new HashMap<>(m);
+            window.clear();            
+            counter.clear();
 
             for (int i = 0; i < m; i++) {
                 int in = scanner.nextInt();
@@ -34,7 +38,7 @@ public class Solution {
             }
 
             int result = counter.size();
-
+      
             for (int i = m; i < n && result < m; i++) {
                 Integer out = window.remove();
                 Integer in = scanner.nextInt();
@@ -47,7 +51,11 @@ public class Solution {
                 }
             }
 
-            System.out.println(result);
+            return result;
         }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(System.in));
     }
 }
---
+++ src/hackerrank/challenges/dataStructures/java_dequeue/SolutionTest.java
@@ -0,0 +1,55 @@
+/**
+ * HackerRank - Java - Data Structures - Java Dequeue
+ * 
+ * @author: Manny egalli64@gmail.com
+ * @see: http://thisthread.blogspot.com/2018/07/hackerrank-java-dequeue.html
+ *       https://www.hackerrank.com/challenges/java-dequeue/problem
+ */
+package hackerrank.challenges.dataStructures.java_dequeue;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.hamcrest.core.Is.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    @Test
+    void testSolutionPlain() {
+        String data = "5 3\n1 2 2 3 3 3";
+        InputStream is = new ByteArrayInputStream(data.getBytes());
+        assertThat(Solution.solution(is), is(2));
+    }
+
+    @Test
+    void testSolutionAll() {
+        String data = "5 3\n1 2 2 3 4 5";
+        InputStream is = new ByteArrayInputStream(data.getBytes());
+        assertThat(Solution.solution(is), is(3));
+    }
+
+    @Test
+    void testSolutionSimple() {
+        String data = "5 3\n1 2 6 3 4 5";
+        InputStream is = new ByteArrayInputStream(data.getBytes());
+        assertThat(Solution.solution(is), is(3));
+    }
+
+    @Test
+    void testSolutionOne() {
+        String data = "5 3\n1 1 1 1 1 1";
+        InputStream is = new ByteArrayInputStream(data.getBytes());
+        assertThat(Solution.solution(is), is(1));
+    }
+
+    @Test
+    void testSolutionWindowTooBig() {
+        String data = "3 5\n1 2 2 3 3 3";
+        InputStream is = new ByteArrayInputStream(data.getBytes());
+        assertThrows(AssertionError.class, () -> Solution.solution(is));
+    }
+
+}
