ac72b5d29e735446e87a1fa1e73e66f53f7aafa7
Emanuele <egalli64@gmail.com>
2025-06-01 10:52:44 +0200

    clone when reference to mutable state

--- src/main/java/effective/ch3/Item13.java
+++ src/main/java/effective/ch3/Item13.java
@@ -13,9 +13,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import effective.ch3.i13.PhoneNumber;
+import effective.ch3.i13.Stack;
 
 /**
  * Override clone judiciously
+ * <p>
+ * When possible, copy constructor or copy factory are preferred approaches.
  */
 public class Item13 {
     private static Logger log = LoggerFactory.getLogger(Item13.class);
@@ -31,10 +34,23 @@ public class Item13 {
         }
     }
 
+    private static void withRefToMutableState() {
+        Stack st1 = new Stack();
+        st1.push(42);
+        Stack st2 = st1.clone();
+
+        if (st1 != st2 && st1.getClass() == st2.getClass() && st1.equals(st2)) {
+            log.info("Not mandatory but expected");
+        } else {
+            log.warn("Unexpected!");
+        }
+    }
+
     public static void main(String[] args) {
         log.trace("Enter");
 
         noRefToMutableState();
+        withRefToMutableState();
 
         log.trace("Exit");
     }
